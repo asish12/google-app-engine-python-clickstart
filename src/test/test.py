aa44b91f36b62c0d1dcee90ee90423e97ef80ef0
Ryan Campbell <ryan.campbell@gmail.com>
2012-09-27 16:50:26 -0500

    fix test

--- src/test/test.py
+++ src/test/test.py
@@ -5,7 +5,7 @@ class SimpleTestCase (unittest.TestCase):
 
     def testSomething(sef):
         """Test basic addition"""
-        assert 1 + 1  == 3, "Arithmetic Unit is broken"
+        assert 1 + 1  == 2, "Arithmetic Unit is broken"
 
 if __name__ == "__main__":
       unittest.main() # run all tests
